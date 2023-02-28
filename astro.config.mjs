b79943eafe72d1592878b3a48e6afff7ce8ad4c1
Katerina Metaxopoulou <kat@katmetax.com>
2023-02-28 23:10:42 +0000

    Make site into PWA

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,47 @@
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro';
 
 // https://astro.build/config
 import react from '@astrojs/react';
 
 // https://astro.build/config
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  output: 'server',
-  adapter: netlify()
+	integrations: [
+		react(),
+		AstroPWA({
+			includeAssets: ['favicon.svg', 'apple-icon-180.png'],
+			manifest: {
+				name: 'Metrolink tram times',
+				short_name: 'MetTramTimes',
+				description: 'Find out when the next Manchester Metrolink tram is',
+				theme_color: '#11191f',
+				icons: [
+					{
+						src: 'manifest-icon-192.maskable.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'manifest-icon-512.maskable.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'manifest-icon-512.maskable.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					},
+				],
+			},
+			devOptions: {
+				enabled: true,
+			},
+		}),
+	],
+	output: 'server',
+	adapter: netlify(),
 });
